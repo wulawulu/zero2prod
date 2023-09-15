a4d90907c62fb5f8a909f6e371a33096ac410404
wu <2473698581@qq.com>
2023-09-15 13:14:23 +0800

    subscriptions相关 进行到3.7.3.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,26 @@
 use std::net::TcpListener;
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpServer, HttpResponse};
 use actix_web::dev::Server;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
