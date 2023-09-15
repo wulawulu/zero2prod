fa4efaf6711d5222c0448244db5405dbf640fe9b
wu <2473698581@qq.com>
2023-09-15 12:17:08 +0800

    完成单元测试 进行到3.7.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
 
@@ -5,12 +6,12 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
     })
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    run(listener)?.await
 }
 
 
