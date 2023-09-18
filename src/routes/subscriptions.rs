1dc69cb0633f90804a8efd6f35a74486479c9af8
wu <2473698581@qq.com>
2023-09-18 16:47:23 +0800

    persistent subscription 进行到3.9.4

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,6 @@
 use actix_web::{HttpResponse, web};
+use sqlx::PgConnection;
+use sqlx::types::chrono::Utc;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -7,6 +9,21 @@ pub struct FormData {
 }
 
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    connection: web::Data<PgConnection>,
+) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1 ,$2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+        .execute(connection.get_ref())
+        .await;
     HttpResponse::Ok().finish()
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,17 @@
 use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
+use sqlx::PgConnection;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
         .listen(listener)?
         .run();
