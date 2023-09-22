9e2880f243127532f7afcc5b052d1c53eb5e5586
wu <2473698581@qq.com>
2023-09-22 16:53:21 +0800

    Going to 4.5.12

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,43 +10,55 @@ pub struct FormData {
     name: String,
 }
 
-
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = % Uuid::new_v4(),
+        subscriber_email = % form.email,
+        subscriber_name = % form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
-        subscribe_email = %form.email,
-        subscribe_name = %form.name,
-    );
-    let _request_span_guard = request_span.enter();
-
     let query_span = tracing::info_span!(
         "Saving new subscriber details in the database"
     );
-    match sqlx::query!(
+    match insert_subscriber( &pool,&form)
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            tracing::error!("Failed to execute query: {:?}",e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[tracing::instrument(
+name = "Adding a new subscriber",
+skip(form, pool)
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    form: &FormData,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1 ,$2, $3, $4)
+     INSERT INTO subscriptions (id, email, name, subscribed_at)
+     VALUES ($1 ,$2, $3, $4)
         "#,
         Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-        .execute(pool.get_ref())
-        .instrument(query_span)
+        .execute(pool)
         .await
-    {
-        Ok(_) => {
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
+        .map_err(|e| {
             tracing::error!("Failed to execute query: {:?}",e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+            e
+        })?;
+    Ok(())
 }
