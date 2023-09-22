db935673d297e748df9dfae7425968a1c2402c36
wu <2473698581@qq.com>
2023-09-22 15:17:38 +0800

    Complete basic tracing. Going to 4.5.11

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@
 pub mod configuration;
 pub mod routes;
 pub mod startup;
-
-
-
+pub mod telemetry;
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use std::net::TcpListener;
-use env_logger::Env;
-use sqlx::{PgPool};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use sqlx::{PgPool};
+use std::net::TcpListener;
+
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+    let listener = TcpListener::bind(address)?;
 
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
+
 
 
