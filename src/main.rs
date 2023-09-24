757adea67b84cc18acbdb325b8b4b38c7b61565f
wutuo <2473698581@qq.com>
2023-09-24 13:53:05 +0800

    Complete dockerfile. Going to chapter 5.3.8.1

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(),std::io::stdout);
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
 
     run(listener, connection_pool)?.await?;
