78bd7abe8a6299defd758e0f877463169ca4cb08
wu <2473698581@qq.com>
2023-09-26 13:29:37 +0800

    change SubscriberName.inner_ref to  as_ref. Going to 6.6.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use sqlx::{PgPool};
 use std::net::TcpListener;
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
 
 
