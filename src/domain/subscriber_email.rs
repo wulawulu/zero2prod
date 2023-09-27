e75a2ec5ea1713d8f59ebbf109f51f3a051d4a52
wu <2473698581@qq.com>
2023-09-27 13:55:52 +0800

    Complete 6.13.4. Going to 6.14.

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -7,8 +7,8 @@ impl SubscriberEmail {
     pub fn parse(s: String) -> Result<SubscriberEmail, String> {
         if validate_email(&s) {
             Ok(Self(s))
-        }else {
-            Err(format!("{} is not a valid subscriber email.",s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
         }
     }
 }
@@ -25,6 +25,7 @@ mod tests {
     use claim::assert_err;
     use fake::Fake;
     use fake::faker::internet::en::SafeEmail;
+    use quickcheck::Gen;
 
     #[test]
     fn empty_string_is_rejected() {
@@ -44,10 +45,19 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        claim::assert_ok!(SubscriberEmail::parse(email));
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
 
