9ccb41621553d33e15141c14f61954325d5925ff
wu <2473698581@qq.com>
2023-09-27 15:12:30 +0800

    Introduce TryFrom/TryInto. Going to 7.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -23,15 +23,10 @@ pub async fn subscribe(
     pool: web::Data<PgPool>,
     form: web::Form<FormData>,
 ) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber { email, name };
     match insert_subscriber(&pool, &new_subscriber).await
     {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -64,4 +59,14 @@ pub async fn insert_subscriber(
             e
         })?;
     Ok(())
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
 }
