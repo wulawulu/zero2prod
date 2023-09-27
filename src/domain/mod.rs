26141707c36bd4996a86b415d5f6675f5e43a6d0
wu <2473698581@qq.com>
2023-09-27 10:10:25 +0800

    Introduce validator to validate email. Going to 6.13

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -2,5 +2,6 @@ mod subscriber_name;
 mod subscriber_email;
 mod new_subscriber;
 
-pub use subscriber_name::SubscriberName;
 pub use new_subscriber::NewSubscriber;
+pub use subscriber_name::SubscriberName;
+pub use subscriber_email::SubscriberEmail;
