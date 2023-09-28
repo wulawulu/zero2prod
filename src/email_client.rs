d5d78d200ec22379e79ec46853d0eb3592bb2274
wu <2473698581@qq.com>
2023-09-28 11:32:42 +0800

    Add test rest client sketch. Going to 7.2.4

--- src/email_client.rs
+++ src/email_client.rs
@@ -25,6 +25,37 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
-        todo!()
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fake::Fake;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::faker::lorem::en::Sentence;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+    use wiremock::matchers::any;
+    use super::*;
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Sentence(1..10).fake();
+
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
     }
 }
