7fc425618cf4650984f7ef85b25e3c83468d31ab
wu <2473698581@qq.com>
2023-10-07 15:19:57 +0800

    7.3.8.1 Extracting startup code.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use zero2prod::configuration::get_configuration;
-use zero2prod::startup::run;
+use zero2prod::startup::build;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use std::net::TcpListener;
-use sqlx::postgres::PgPoolOptions;
-use zero2prod::email_client::EmailClient;
 
 
 #[tokio::main]
@@ -12,24 +9,9 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
 
-    let sender_email = configuration.email_client.sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
-    let listener = TcpListener::bind(address)?;
-
-    run(listener, connection_pool, email_client)?.await?;
+    let server = build(configuration).await?;
+    server.await?;
     Ok(())
 }
 
--- src/startup.rs
+++ src/startup.rs
@@ -4,9 +4,36 @@ use actix_web::{App, HttpServer, web};
 use actix_web::web::Data;
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
+use sqlx::postgres::PgPoolOptions;
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 
 use crate::routes::{health_check, subscribe};
+
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool, email_client)
+}
 
 pub fn run(
     listener: TcpListener,
@@ -27,3 +54,5 @@ pub fn run(
         .run();
     Ok(server)
 }
+
+
