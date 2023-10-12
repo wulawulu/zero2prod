86dbbdc90d0eb05f37fbede664c43cfac19b3ce9
wu <2473698581@qq.com>
2023-10-12 14:55:03 +0800

    Complete 7.7.2 send a static confirmation link. Going to 7.7.3.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -31,19 +31,42 @@ pub async fn subscribe(
     if insert_subscriber(&pool, &new_subscriber).await.is_err(){
         return HttpResponse::InternalServerError().finish()
     }
-    if email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
-        )
+   if send_confirmation_email(&email_client, new_subscriber)
         .await
         .is_err()
     {
         return HttpResponse::InternalServerError().finish()
     }
     HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(
+    name = "Send a confirmation email to a new subscriber",
+    skip(email_client, new_subscriber)
+)]
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link =
+        "https://my-api.com/subscriptions/confirm";
+    let plain_text = format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        confirmation_link
+    );
+    let html_text = format!(
+        "Welcome to our newsletter!<br />\
+                Click <a href=\"{}\"here</a> to confirm your subscription.",
+        confirmation_link
+    );
+    email_client
+        .send_email(
+            new_subscriber.email,
+            "Welcome!",
+            &html_text,
+            &plain_text,
+        )
+        .await
 }
 
 #[tracing::instrument(
