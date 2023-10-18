f579d8918d3e95ab34df04e4bc2c31ccf8541058
wu <2473698581@qq.com>
2023-10-18 13:55:08 +0800

    Complete refactoring subscriptions_confirm.rs. Going to Chapter 9.

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,73 +1,79 @@
-use actix_web::{HttpResponse, web};
+use std::fmt::Formatter;
+use actix_web::{HttpResponse, ResponseError, web};
+use actix_web::http::StatusCode;
+use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
+use crate::routes::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 pub struct Parameters {
     subscription_token: String,
 }
 
-#[tracing::instrument(
-    name = "Confirm a pending subscriber",
-    skip(parameters, pool)
-)]
-pub async fn confirm(
-    parameters: web::Query<Parameters>,
-    pool: web::Data<PgPool>,
-) -> HttpResponse {
-    let id = match get_subscriber_id_from_token(&pool,&parameters.subscription_token).await {
-        Ok(id)=>id,
-        Err(_)=>return HttpResponse::InternalServerError().finish(),
-    };
-    match id {
-        None => HttpResponse::Unauthorized().finish(),
-        Some(subscriber_id)=>{
-            if confirm_subscriber(&pool,subscriber_id).await.is_err(){
-                return HttpResponse::InternalServerError().finish();
-            }
-            HttpResponse::Ok().finish()
+#[derive(thiserror::Error)]
+pub enum ConfirmationError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+    #[error("There is no subscriber associated with the provided token.")]
+    UnknownToken,
+}
+
+impl std::fmt::Debug for ConfirmationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for ConfirmationError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            ConfirmationError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ConfirmationError::UnknownToken => StatusCode::UNAUTHORIZED,
         }
     }
 }
 
-#[tracing::instrument(
-    name="Mark subscriber as confirmed",
-    skip(subscriber_id,pool)
-)]
+#[tracing::instrument(name = "Confirm a pending subscriber", skip(parameters, pool))]
+pub async fn confirm(
+    parameters: web::Query<Parameters>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, ConfirmationError> {
+    let subscriber_id = get_subscriber_id_from_token(&pool, &parameters.subscription_token)
+        .await
+        .context("Failed to find subscriber id by token in database.")?
+        .ok_or(ConfirmationError::UnknownToken)?;
+    confirm_subscriber(&pool, subscriber_id)
+        .await
+        .context("Failed to confirm subscriber in database.")?;
+    Ok(HttpResponse::Ok().finish())
+}
+
+
+#[tracing::instrument(name = "Mark subscriber as confirmed", skip(subscriber_id, pool))]
 pub async fn confirm_subscriber(
     pool: &PgPool,
-    subscriber_id: Uuid
-) -> Result<(),sqlx::Error>{
+    subscriber_id: Uuid,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"UPDATE subscriptions SET status = 'confirmed' WHERE id = $1"#,
         subscriber_id,
     )
         .execute(pool)
-        .await
-        .map_err(|e|{
-            tracing::error!("Failed to execute query: {:?}",e);
-            e
-        })?;
+        .await?;
     Ok(())
 }
 
-#[tracing::instrument(
-    name="Get subscriber_id from token",
-    skip(subscription_token,pool)
-)]
+#[tracing::instrument(name = "Get subscriber_id from token", skip(subscription_token, pool))]
 pub async fn get_subscriber_id_from_token(
     pool: &PgPool,
-    subscription_token: &str
-) -> Result<Option<Uuid>,sqlx::Error> {
+    subscription_token: &str,
+) -> Result<Option<Uuid>, sqlx::Error> {
     let result = sqlx::query!(
         r#"SELECT subscriber_id FROM subscription_tokens WHERE subscription_token = $1"#,
         subscription_token,
     )
         .fetch_optional(pool)
-        .await
-        .map_err(|e| {
-            tracing::error!("Failed to execute query: {:?}", e);
-            e
-        })?;
-    Ok(result.map(|r|r.subscriber_id))
+        .await?;
+    Ok(result.map(|r| r.subscriber_id))
 }
