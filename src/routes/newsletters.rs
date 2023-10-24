47049ebf958e0672c8f2eb99e06eb77679d5f629
wu <2473698581@qq.com>
2023-10-24 15:05:21 +0800

    Complete 10.2.3.2. Going to 10.2.3.6.

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -4,6 +4,7 @@ use actix_web::http::header::{HeaderMap, HeaderValue};
 use actix_web::http::{header, StatusCode};
 use anyhow::Context;
 use secrecy::{ExposeSecret, Secret};
+use sha3::Digest;
 use sqlx::PgPool;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
@@ -108,14 +109,18 @@ struct Credentials {
 }
 
 async fn validate_credentials(credentials: Credentials, pool: &PgPool) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(
+        credentials.password.expose_secret().as_bytes()
+    );
+    let password_hash= format!("{:x}",password_hash);
     let user_id: Option<_> = sqlx::query!(
         r#"
         SELECT user_id
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 AND password_hash = $2
         "#,
         credentials.username,
-        credentials.password.expose_secret()
+        password_hash
     )
         .fetch_optional(pool)
         .await
