01fa2ef26cf7681c8273a3c97e83a213f647a42d
wu <2473698581@qq.com>
2023-12-22 14:53:20 +0800

    1.format shell script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,40 +1,56 @@
 #!/usr/bin/env bash
+
+# 开启调试模式
 set -x
+
+# 任何命令执行失败都将导致脚本终止执行
 set -eo pipefail
+
+# 检查是否安装了 psql
 if ! [ -x "$(command -v psql)" ]; then
-echo >&2 "Error: psql is not installed."
-exit 1
+    echo >&2 "Error: psql is not installed."
+    exit 1
 fi
+
+# 检查是否安装了 sqlx
 if ! [ -x "$(command -v sqlx)" ]; then
-echo >&2 "Error: sqlx is not installed."
-echo >&2 "Use:"
-echo >&2 " cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
-echo >&2 "to install it."
-exit 1
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "Use:"
+    echo >&2 " cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+    echo >&2 "to install it."
+    exit 1
 fi
+
+# 设置数据库配置
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Allow to skip Docker if a dockerized Postgres database is already running
+# 检查是否跳过 Docker 启动
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-docker run \
--e POSTGRES_USER=${DB_USER} \
--e POSTGRES_PASSWORD=${DB_PASSWORD} \
--e POSTGRES_DB=${DB_NAME} \
--p "${DB_PORT}":5432 \
--d postgres \
-postgres -N 1000
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
 fi
+
+# 等待 Postgres 数据库启动
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
->&2 echo "Postgres is still unavailable - sleeping"
-sleep 1
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
 done
+
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+
+# 运行数据库迁移
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
 sqlx migrate run
+
 >&2 echo "Postgres has been migrated, ready to go!"
