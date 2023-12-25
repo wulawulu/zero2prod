60eeb1e640f0e0cc73f580385c76c9a803aaef5f
wu <2473698581@qq.com>
2023-12-25 13:04:40 +0800

    1.chapter 10.2.5

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,3 +1,4 @@
+use uuid::Uuid;
 use wiremock::matchers::{any, path, method};
 use wiremock::{Mock, ResponseTemplate};
 use crate::helpers::{ConfirmationLinks, spawn_app, TestApp};
@@ -135,4 +136,32 @@ async fn requests_missing_authorization_are_rejected() {
     // Assert
     assert_eq!(401, response.status().as_u16());
     assert_eq!(r#"Basic realm="publish""#, response.headers()["WWW-Authenticate"]);
+}
+
+#[tokio::test]
+async fn non_existing_user_is_rejected() {
+    let app = spawn_app().await;
+    // let username = Uuid::new_v4().to_string();
+    let username = &app.test_user.username;
+    let password = Uuid::new_v4().to_string();
+    assert_ne!(app.test_user.password, password);
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>",
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
 }
