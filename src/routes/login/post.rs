8c63dcc91c2f7aed4157f1bf402f223ad20b7515
wu <2473698581@qq.com>
2023-12-27 16:43:58 +0800

    1.认证失败后重定向到登录页面并且页面动态返回错误信息

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -54,10 +54,14 @@ impl std::fmt::Debug for LoginError {
 }
 
 impl ResponseError for LoginError {
+    fn error_response(&self)->HttpResponse{
+        let encoded_error = urlencoding::Encoded::new(self.to_string());
+        HttpResponse::build(self.status_code())
+            .insert_header((LOCATION, format!("/login?error={}", encoded_error)))
+            .finish()
+    }
+
     fn status_code(&self) -> StatusCode {
-        match self {
-            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
-            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-        }
+        StatusCode::SEE_OTHER
     }
 }
