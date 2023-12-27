593727d953f8f933db4bfad67fb9473d621f6f76
wu <2473698581@qq.com>
2023-12-27 11:28:23 +0800

    1.完成基本登录验证,将要进行到10.6.4

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,11 @@
+use std::fmt::Formatter;
 use actix_web::http::header::LOCATION;
-use actix_web::{HttpResponse, web};
+use actix_web::{HttpResponse, ResponseError, web};
+use actix_web::http::StatusCode;
 use secrecy::Secret;
+use sqlx::PgPool;
+use crate::authentication::{AuthError, Credentials, validate_credentials};
+use crate::routes::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -8,8 +13,51 @@ pub struct FormData {
     password: Secret<String>,
 }
 
-pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
+#[tracing::instrument(
+skip(form, pool),
+fields(username = tracing::field::Empty, user_id = tracing::field::Empty)
+)]
+pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Result<HttpResponse, LoginError> {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+    tracing::Span::current()
+        .record("username", &tracing::field::display(&credentials.username));
+    let user_id = validate_credentials(credentials, &pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
+    tracing::Span::current()
+        .record("user_id", &tracing::field::display(&user_id));
+
+
+    Ok(HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
-        .finish()
+        .finish())
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
