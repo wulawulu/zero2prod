cc0c0088129bc94715f9ca428a2a7247c3180e0c
zoso <w2473698581@qq.com>
2023-12-27 22:38:36 +0800

    完成对重定向url的tag参数进行hmac,一边服务端在处理get /login请求时确定其身份,防止xss

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
 use actix_web::web::Data;
+use secrecy::Secret;
 use sqlx::{PgPool, Pool, Postgres};
 use tracing_actix_web::TracingLogger;
 use sqlx::postgres::PgPoolOptions;
@@ -43,6 +44,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret
         )?;
 
         Ok(Self { port, server })
@@ -72,6 +74,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret:Secret<String>
 ) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = Data::new(email_client);
@@ -89,10 +92,12 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
         .listen(listener)?
         .run();
     Ok(server)
 }
 
-
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
