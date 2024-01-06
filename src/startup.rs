af6b938cb06193bc4dc0fbd93e71c654a0d79f90
wulawulu <w2473698581@163.com>
2024-01-06 11:47:48 +0800

    1.增加logout endpoint 2.测试登录 退出流程

--- src/startup.rs
+++ src/startup.rs
@@ -14,7 +14,7 @@ use sqlx::{PgPool, Pool, Postgres};
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-use crate::routes::{admin_dashboard, change_password, change_password_form, confirm, health_check, home, login, login_form, publish_newsletter, subscribe};
+use crate::routes::{admin_dashboard, change_password, change_password_form, confirm, health_check, home, log_out, login, login_form, publish_newsletter, subscribe};
 
 pub struct Application {
     port: u16,
@@ -107,6 +107,7 @@ async fn run(
             .route("/admin/dashboard", web::get().to(admin_dashboard))
             .route("/admin/password", web::get().to(change_password_form))
             .route("/admin/password", web::post().to(change_password))
+            .route("/admin/logout", web::post().to(log_out))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
