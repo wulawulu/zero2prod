a96824d63825c33cbf05e6c3716e9f78231215c1
wu <2473698581@qq.com>
2024-01-08 13:53:20 +0800

    1.并发单元测试

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,3 +1,4 @@
+use std::time::Duration;
 use crate::helpers::{assert_is_redirect_to, spawn_app, ConfirmationLinks, TestApp};
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
@@ -174,4 +175,32 @@ async fn newsletter_creation_is_idempotent(){
     // );
     // Mock verifies on Drop that we have sent the newsletter email **once**
 
+}
+
+#[tokio::test]
+async fn concurrent_form_submission_is_handled_gracefully(){
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    app.login().await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        // Setting a long delay to ensure that the second request
+        // arrives before the first one completes
+        .respond_with(ResponseTemplate::new(200).set_delay(Duration::from_secs(2)))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title":"Newsletter title",
+        "text_content":"Newsletter body as plain text",
+        "html_content":"<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string()
+    });
+    let response1 = app.post_publish_newsletter(&newsletter_request_body);
+    let response2 = app.post_publish_newsletter(&newsletter_request_body);
+    let (response1,response2) = tokio::join!(response1,response2);
+    assert_eq!(response1.status(), response2.status());
+    assert_eq!(response1.text().await.unwrap(), response2.text().await.unwrap());
 }
